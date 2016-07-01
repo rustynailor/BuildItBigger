0b1b2954f135631e4193e0bcb9f45dbf9d8c9572
rustynailor <russell@scapegrace.co.uk>
2016-07-01 16:38:11 +0100

    Android library added

---
+++ joketeller/src/main/java/com/rantmedia/joketeller/TellJokeFragment.java
@@ -0,0 +1,22 @@
+package com.rantmedia.joketeller;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class TellJokeFragment extends Fragment {
+
+    public TellJokeFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tell_joke, container, false);
+    }
+}
