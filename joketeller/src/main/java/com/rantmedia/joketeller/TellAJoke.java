3688e203f675f96f53367f527da904593d980979
rustynailor <russell@scapegrace.co.uk>
2016-07-01 16:59:39 +0100

    TellAJoke activity added without fragment

--- joketeller/src/main/java/com/rantmedia/joketeller/TellAJoke.java
+++ joketeller/src/main/java/com/rantmedia/joketeller/TellAJoke.java
@@ -1,29 +1,34 @@
 package com.rantmedia.joketeller;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
-public class TellJoke extends AppCompatActivity {
+public class TellAJoke extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tell_joke);
+        setContentView(R.layout.activity_tell_ajoke);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        TextView jokeView = (TextView) findViewById(R.id.jokeView);
+
+        //get joke from intent extra
+        Intent intent = getIntent();
+        String joke = intent.getStringExtra("joke");
+
+        if(joke != null){
+            jokeView.setText(joke);
+        }
+
+
     }
 
 }
