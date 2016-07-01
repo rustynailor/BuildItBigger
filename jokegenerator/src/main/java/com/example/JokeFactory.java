e512dd11b3f264ae28b4b4a9944b200f48d1068c
rustynailor <russell@scapegrace.co.uk>
2016-07-01 14:45:15 +0100

    Java based joke telling library added';

---
+++ jokegenerator/src/main/java/com/example/JokeFactory.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class JokeFactory {
+
+    private String testJoke = "My dog has no nose. How does he smell? Terrible!";
+
+    public String getJoke() {
+        return testJoke;
+    }
+}
